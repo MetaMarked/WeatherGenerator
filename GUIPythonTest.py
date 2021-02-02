efe3966303a212f0737157f54e5deadd5299ac77
MetaMarked <Belmont@MetaMarked.com>
2021-02-02 18:33:11 -0500

    GUI commit

--- GUIPythonTest.py
+++ GUIPythonTest.py
@@ -1,19 +1,30 @@
 import tkinter as tk
 
-Height = 700
-Width = 800
+HEIGHT = 700
+WIDTH = 800
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, height=Height, width=Width)
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root)
-frame.place(relwidth=1 , relheight=1) #relx = 0.1 , rely = 0.1 centers item
-button = tk.Button(frame, text = "Login", bg="Purple")
-button.pack()
+#background_image= tk.PhotoImage(file='DragonDive.png') 
+#background_label= tk.Label(root, image=background_image)
+#background_label.place(relwidth=1, relheight=1)
 
-label = tk.Label(frame, text="This is a label", bg='yellow')
-label.pack()
+frame = tk.Frame(root, bg='#d12dfd', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75 , relheight=0.1, anchor='n') #relx = 0.1 , rely = 0.1 centers item
 
-root.mainloop()
+entry = tk.Entry(frame, bg='light blue', font=40)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text = "Get Weather", bg="Purple")
+button.place(relx=0.7, relwidth=0.3, relheight=1)
+
+lower_frame = tk.Frame(root, bg='#d12dfd', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75 , relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, text="This is a label", bg='light blue')
+label.place( relwidth=1, relheight=1)
+
+root.mainloop() 
